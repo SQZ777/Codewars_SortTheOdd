b9d8cbaf036397691abc78e8c9c560429ee7dc4b
sqz777 <sqz777isme@gmail.com>
2017-07-22 18:20:00 +0800

    input 1 2 3 Should be 3 2 1 failed

--- SortTheOddTest/SortTheOddTest/kata.cs
+++ SortTheOddTest/SortTheOddTest/kata.cs
@@ -13,10 +13,17 @@ namespace SortTheOddTest
             sortResult(new[] { 0 }, new[] { 0 });
         }
 
+
         [TestMethod]
-        public void Input_3_2_1_Sould_Be_3_2_1()
+        public void Input_3_2_1_Should_Be_3_2_1()
         {
             sortResult(new[] { 3, 2, 1 }, new[] { 3, 2, 1 });
+        }
+
+        [TestMethod]
+        public void Input_1_2_3_Should_Be_3_2_1()
+        {
+            sortResult(new[] { 1, 2, 3 }, new[] { 3, 2, 1 });
         }
 
         private static void sortResult(int[] expected, int[] input)
