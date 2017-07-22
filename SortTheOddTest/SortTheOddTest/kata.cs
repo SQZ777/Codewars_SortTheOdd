fa2ad59afa716dc7942c42e28eccde23803df778
sqz777 <sqz777isme@gmail.com>
2017-07-22 18:17:40 +0800

    Test build complete test input 0 should be 0 complete test input 3 2 1 should be 3 2 1 complete

---
+++ SortTheOddTest/SortTheOddTest/kata.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SortTheOddTest
+{
+    [TestClass]
+    public class kata
+    {
+        [TestMethod]
+        public void Input_0_Should_Be_0()
+        {
+            sortResult(new[] { 0 }, new[] { 0 });
+        }
+
+        [TestMethod]
+        public void Input_3_2_1_Sould_Be_3_2_1()
+        {
+            sortResult(new[] { 3, 2, 1 }, new[] { 3, 2, 1 });
+        }
+
+        private static void sortResult(int[] expected, int[] input)
+        {
+            var kata = new Kata();
+            var actual = kata.sortTheOdd(input);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public int[] sortTheOdd(int[] input)
+        {
+
+            return input;
+        }
+    }
+}
